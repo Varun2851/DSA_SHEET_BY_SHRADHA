b304150f82701f86f4f843f2fcccbf319324c2c4
Varun Yadav <varunyadav891@gmail.com>
2022-08-05 01:07:21 +0530

    Added

---
+++ Greedy/Maximize_Sum_afterK_negations.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	int arr[] = {9, 8, 8, 5};
+	int n = sizeof(arr)/sizeof(int);
+
+	int k = 3;
+
+	sort(arr,arr+n); //sorting is necessary
+
+	int sum = 0;
+
+	int index = 0;
+
+	for(index = 0; index<n; index++){
+		if(k != 0 and arr[index] < 0){
+			arr[index] = -1*arr[index];
+			k--;
+		}
+		else{
+			break;
+		}
+	}
+	//if all numbers are positive
+	if(index == 0){
+		index = 1;
+	}
+	//if all numbers are negative
+	if(index == n){
+		index = -1;
+	}
+	if(k %2 == 0 or k == 0){
+		for(int i = 0; i<n; i++){
+			sum += arr[i];
+		}
+	}
+
+	if(abs(arr[index]) > arr[index-1]){
+		index--;
+	}
+	arr[index] = -1*arr[index];
+
+	for(int i = 0; i<n; i++){
+		sum += arr[i];
+	}
+
+	cout<<sum<<" ";
+
+	return 0;
+}
